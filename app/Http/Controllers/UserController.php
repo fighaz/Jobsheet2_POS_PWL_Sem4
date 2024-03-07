838cdfeeb10823e148412a110abf16861e17a4bd
fighaz <sofisugiartozaini@gmail.com>
2024-03-07 12:45:05 +0700

    retrieving or creating

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,16 +10,13 @@ class UserController extends Controller
 {
     //
     public function index(){
-        // $data = [
-        //     'level_id' => 2,
-        //     'username' => 'manager_tiga',
-        //     'nama' => 'Manager 3',
-        //     'password' => Hash::make('12345')
-        // ];
-        // UserModel::create($data);
-
-        // $user = UserModel::all();
-        $user = UserModel::where('username','manager9')->firstOrFail();
+        $user = UserModel::firstOrNew([
+            'username' => 'manager33',
+            'nama' => 'Manager Tiga Tiga',
+            'password' => Hash::make('12345'),
+            'level_id' => 2
+        ]);
+        $user->save();
         return view('user',['data'=>$user]);
     }
 }
--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -10,6 +10,6 @@ class UserModel extends Model
     use HasFactory;
     protected $table = 'm_user';
     protected $primaryKey ='user_id';
-    protected $fillable = ['level_id','username','nama'];
+    protected $fillable = ['level_id','username','nama','password'];
 
 }
