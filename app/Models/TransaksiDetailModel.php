4a475397e50ea215de15f772067c836ea5737a40
fighaz <sofisugiartozaini@gmail.com>
2024-04-05 09:27:44 +0700

    fix transaksi

--- app/Models/TransaksiDetailModel.php
+++ app/Models/TransaksiDetailModel.php
@@ -8,4 +8,29 @@ use Illuminate\Database\Eloquent\Model;
 class TransaksiDetailModel extends Model
 {
     use HasFactory;
+
+    // table name, primary key, and other property related to database
+    protected $table = 't_penjualan_detail';
+    protected $primaryKey = 'detail_id';
+
+    // fillable field
+    protected $fillable = [
+        'detail_id',
+        'penjualan_id',
+        'barang_id',
+        'jumlah',
+        'harga'
+    ];
+
+    // relationship with barang
+    public function barang()
+    {
+        return $this->belongsTo(BarangModel::class, 'barang_id', 'barang_id');
+    }
+
+    // relationship with penjualan
+    public function penjualan()
+    {
+        return $this->belongsTo(TransaksiModel::class, 'penjualan_id', 'penjualan_id');
+    }
 }
